010955a6dab57679e75908614cae7eca41e2d847
dpisati <43629737+dpisati@users.noreply.github.com>
2020-09-16 20:53:07 +1200

    3day

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+import './styles.css';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 export default function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Awesome! You want to give classes" />
+            <PageHeader 
+                title="Awesome! You want to give classes" 
+                description="The first step is filling this form."
+            />
+            <main>
+                <fieldset>
+                    <legend>Your details</legend>
+                    <Input name="name" label="Name" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografy"/>  
+                </fieldset>
+                <fieldset>
+                    <legend>About the Class</legend>
+                    <Select 
+                        name="subject" 
+                        label="Subject"
+                        options={[
+                            { value: 'Arts', label: 'Arts' },
+                            { value: 'Design', label: 'Design' },
+                            { value: 'Math', label: 'Math' }
+                        ]}
+                    />
+                    <Input name="price" label="Your Price per Hour" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>Availability</legend>
+                    <button type="button">
+                        + Add time
+                    </button>
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Warning Icon"/>
+                    Important! <br />
+                    Fill all the fields.
+                    </p>
+                    <button type="button">
+                        Save
+                    </button>
+                </footer>
+
+            </main>
         </div>
     )
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
 
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="These are the available Proffys">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Subject</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Week-Day</label>
-                        <input type="text" id="week-day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Time</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Select 
+                        name="subject" 
+                        label="Subject"
+                        options={[
+                            { value: 'Arts', label: 'Arts' },
+                            { value: 'Design', label: 'Design' },
+                            { value: 'Math', label: 'Math' }
+                        ]}
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Week Day"
+                        options={[
+                            { value: '0', label: 'Sunday' },
+                            { value: '1', label: 'Monday' },
+                            { value: '2', label: 'Tuesday' },
+                            { value: '3', label: 'Wednesday' },
+                            { value: '4', label: 'Thursday' },
+                            { value: '5', label: 'Friday' },
+                            { value: '6', label: 'Saturday' }
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Time" />
                 </form>
             </PageHeader>
 
